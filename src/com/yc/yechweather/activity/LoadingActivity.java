3b17f59201b125b1930c0de09cf35286b7915e0f
yechen <1522120424@qq.com>
2015-09-26 13:19:16 +0800

    增加了启动应用的界面

---
+++ src/com/yc/yechweather/activity/LoadingActivity.java
@@ -0,0 +1,35 @@
+package com.yc.yechweather.activity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.yc.yechweather.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * 程序进入时的加载页
+ * @author Administrator
+ *
+ */
+public class LoadingActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.loading_layout);
+		new Timer().schedule(new TimerTask() {  
+            @Override  
+            public void run() {  
+                startActivity(new Intent(LoadingActivity.this, WeatherActivity.class));  
+                finish();  
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);  
+            }  
+        }, 2000);  
+	}
+
+}
